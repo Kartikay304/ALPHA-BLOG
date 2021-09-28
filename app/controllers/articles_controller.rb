1752f08165692df7e1e40b64075597ad1a52f1da
Kartikay Kala <kartikay304@gmail.com>
2021-09-29 00:22:39 +0530

    Added edit article form and update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,11 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
     def create 
         # render plain: params[:article]
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -23,6 +28,17 @@ class ArticlesController < ApplicationController
             # redirect_to article_path(@article)
         else 
             render 'new'
+        end
+    end
+
+    def update
+        # byebug
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
         end
     end
 
